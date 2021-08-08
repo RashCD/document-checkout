8771634b13612a72003732f182cee291c61d5431
CD <mrrash91@gmail.com>
2021-08-09 06:29:16 +0800

    add checkout page title

--- src/views/CheckoutPage.tsx
+++ src/views/CheckoutPage.tsx
@@ -1,9 +1,11 @@
 import { RouteComponentProps } from '@reach/router';
 import React from 'react';
+import AppLayout from '../components/AppLayout';
 
 const CheckoutPage = (props: RouteComponentProps) => {
   return (
     <div>
+      <AppLayout title="Checkout" />
       <p>CheckoutPage</p>
     </div>
   );
