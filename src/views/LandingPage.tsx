8ce6958e91b2dbc2c5409a78076c97851636a470
CD <mrrash91@gmail.com>
2021-08-09 02:10:45 +0800

    add cookie to product list and cart icon

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,12 +1,15 @@
 import { RouteComponentProps } from '@reach/router';
 import ProductList from '../components/ProductList';
+import Icon from '../components/Icon';
 import Styles from '../assets/styles/views/LandingPage.module.scss';
+import CartIcon from '../assets/icons/cart.svg';
 
 const LandingPage = (props: RouteComponentProps) => {
   return (
     <div className={Styles.app}>
       <header className={Styles.appHeader}>
         <h1>{process.env.REACT_APP_WEBSITE_NAME}</h1>
+        <Icon src={CartIcon} size={38} color="white" alt="cart icon" />
       </header>
       <main className={Styles.appMain}>
         <ProductList />
