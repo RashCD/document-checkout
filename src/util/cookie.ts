1ded3c0eaf1061e5fb323861e8b32f2818808977
CD <mrrash91@gmail.com>
2021-08-09 02:09:02 +0800

    add eslint global and cookie util

---
+++ src/util/cookie.ts
@@ -0,0 +1,50 @@
+import Cookies from 'js-cookie';
+
+class Cookie {
+  static DEFAULT_OPTIONS = {
+    path: '/',
+    secure: window.location.protocol === 'https:',
+    domain: window.location.hostname || '',
+    expires: 365,
+  };
+
+  static set(key: string, value: string, options = {}) {
+    return Cookies.set(key, value, {
+      ...Cookie.DEFAULT_OPTIONS,
+      ...options,
+    });
+  }
+
+  static append<T>(key: string, value: T, options = {}) {
+    const cookieValue = Cookie.get(key, '');
+
+    if (cookieValue) {
+      const cookieValueArray: T[] = JSON.parse(cookieValue);
+
+      const cookieExist = cookieValueArray.some((cookie) => {
+        return JSON.stringify(cookie) === JSON.stringify(value);
+      });
+
+      const mergeCookieValue = [...cookieValueArray, value];
+
+      !cookieExist &&
+        Cookie.set(key, JSON.stringify(mergeCookieValue), options);
+    } else {
+      Cookie.set(key, JSON.stringify([value]));
+    }
+  }
+
+  static get(key: string, defaultValue: string) {
+    return Cookies.get(key) || defaultValue;
+  }
+
+  static getAll() {
+    return Cookies.get();
+  }
+
+  static remove(key: string, options = {}) {
+    return Cookies.remove(key, { ...Cookie.DEFAULT_OPTIONS, ...options });
+  }
+}
+
+export default Cookie;
