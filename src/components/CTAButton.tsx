f601fec174cb3bc222b271f7a95d8d76fdf4be5c
CD <mrrash91@gmail.com>
2021-08-09 08:28:52 +0800

    couple of adjustment on cta button and variables

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button, { buttonTypes } from '../components/Button';
 import Styles from '../assets/styles/components/CTAButton.module.scss';
+import mergeClass from 'clsx';
 
 const CTAButton = (props: buttonTypes) => {
-  const { children, ...rest } = props;
+  const { children, className, ...rest } = props;
   return (
-    <Button className={Styles.cta} {...rest}>
+    <Button className={mergeClass(Styles.cta, className)} {...rest}>
       {children}
     </Button>
   );
