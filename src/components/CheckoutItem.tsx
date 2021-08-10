9211f4e40f9244a010d3c73288b7fec9e34c5cc4
CD <mrrash91@gmail.com>
2021-08-10 08:47:01 +0800

    add logic & conditional for promocode

--- src/components/CheckoutItem.tsx
+++ src/components/CheckoutItem.tsx
@@ -23,10 +23,13 @@ type checkoutItemTypes = {
 
 const CheckoutItem = (props: checkoutItemTypes) => {
   const { id, title, price, currency, promo, image } = props;
-  const [count, setCount] = useState(0);
   const { deleteProduct, setTotal } = useContext(CartContext);
+  const [count, setCount] = useState(0);
+  const [promoText, setPromoText] = useState('');
+  const [isPromoSuccess, setIsPromoSuccess] = useState(false);
 
-  const totalPrice = priceString.format(count * Number(price));
+  const priceAfterDiscount = isPromoSuccess ? promo?.discountPrice : price;
+  const totalPrice = priceString.format(count * Number(priceAfterDiscount));
   const hasPromocode = !!promo?.code;
   const purchaseCountRequired = promo?.eligibility.purchaseItem || 0;
   const displayPurchaseCountEligibility = purchaseCountRequired - count;
@@ -43,12 +46,29 @@ const CheckoutItem = (props: checkoutItemTypes) => {
     }
   }, [count, id, setTotal, totalPrice]);
 
+  useEffect(() => {
+    if (count < (promo?.eligibility.purchaseItem || false))
+      return setIsPromoSuccess(false);
+  }, [count, promo?.eligibility.purchaseItem]);
+
+  const applyPromocode = () => {
+    if (!promoText) {
+      alert('You did not enter any promocode.');
+    } else if (count < (promo?.eligibility.purchaseItem || false)) {
+      alert('You did not meet the requirements.');
+    } else if (promoText !== promo?.code) {
+      alert('You entered a wrong promocode. Please try again.');
+    } else {
+      return setIsPromoSuccess(true);
+    }
+  };
+
   return (
     <div className={Styles.checkoutItem}>
       <div className={Styles.checkoutTop}>
         <p className={Styles.checkoutIndicator}>
           {hasPromocode &&
-            `Save ${currency}${promo?.eligibility.discount} with promocode`}
+            `Save ${currency}${promo?.eligibility.discount} with promocode ${promo?.code}`}
         </p>
         <CTAButton
           className={Styles.checkoutDelete}
@@ -65,10 +85,25 @@ const CheckoutItem = (props: checkoutItemTypes) => {
         />
         <div className={Styles.checkoutDetails}>
           <p className={Styles.checkoutTitle}>{title}</p>
-          <p className={Styles.checkoutPrice}>
-            {currency}
-            {price}
-          </p>
+          <div className={Styles.checkoutPrice}>
+            {isPromoSuccess ? (
+              <div className={Styles.checkoutPriceDiscount}>
+                <del>
+                  {currency}
+                  {price}
+                </del>
+                <p>
+                  {currency}
+                  {promo?.discountPrice}
+                </p>
+              </div>
+            ) : (
+              <p>
+                {currency}
+                {price}
+              </p>
+            )}
+          </div>
           <div className={Styles.checkoutCount}>
             <Button
               onButtonClick={() => setCount((count) => (count ? count - 1 : 0))}
@@ -96,16 +131,35 @@ const CheckoutItem = (props: checkoutItemTypes) => {
           </div>
         </div>
       </div>
-      <div className={Styles.checkoutBottom}>
-        {hasPromocode && (
-          <p>
+      {hasPromocode && (
+        <div className={Styles.checkoutBottom}>
+          <p className={Styles.checkoutPromoText}>
             {displayPurchaseCountEligibility > 0
-              ? `Purchase another ${displayPurchaseCountEligibility} items to received
+              ? `Purchase another ${displayPurchaseCountEligibility} items to receive
             discount`
-              : 'You are eligible to receive discount. Click add promocode to apply to your purchase'}
+              : 'You are eligible to receive discount. You can apply promocode to your purchase'}
           </p>
-        )}
-      </div>
+          <div className={Styles.checkoutPromo}>
+            {isPromoSuccess ? (
+              <p className={Styles.promoApplied}>Promocode Applied</p>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  className={Styles.checkoutPromoInput}
+                  onChange={(event) => setPromoText(event.currentTarget.value)}
+                />
+                <CTAButton
+                  className={Styles.checkoutPromoButton}
+                  onButtonClick={applyPromocode}
+                >
+                  Add Promocode
+                </CTAButton>
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
